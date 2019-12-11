f04654ed24373e11647075321d18070755f14561
Igor Pestana <igor.pestana@b2wdigital.com>
2019-12-11 17:35:54 -0300

    First

---
+++ src/main/java/br/com/restmonitoring/RestMonitoringApplication.java
@@ -0,0 +1,13 @@
+package br.com.restmonitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestMonitoringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/restmonitoring/RestMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.restmonitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
